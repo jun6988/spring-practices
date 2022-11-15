7b559c1e5340e8f26884f7d758c188068546f15a
jun6988 <114986356+jun6988@users.noreply.github.com>
2022-11-15 11:49:42 +0900

    @RequestMapping & @RequestParam

--- helloweb03/src/main/java/com/bitacademy/helloweb/controller/UserController.java
+++ helloweb03/src/main/java/com/bitacademy/helloweb/controller/UserController.java
@@ -2,26 +2,51 @@ package com.bitacademy.helloweb.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
- * 
  * @RequestMapping 클래스 + 메소드
- * 이걸 추천
+ * 추천
+ *
  */
 @Controller
 @RequestMapping("/user")
 public class UserController {
+	
+	@RequestMapping(value="/join", method=RequestMethod.GET)
+	public String join() {
+		return "/WEB-INF/views/join.jsp";
+	}
 
-	@ResponseBody
-	@RequestMapping("/joinform")
-	public String joinform() {
-		return "UserController.joinform";		
+	@RequestMapping(value="/join", method=RequestMethod.POST)
+	public String join(UserVo vo) {
+		System.out.println(vo);
+		return "redirect:/";
 	}
 	
 	@ResponseBody
-	@RequestMapping("/join")
-	public String join() {
-		return "UserController.join";		
+	@RequestMapping("/update")
+	public String update(@RequestParam("n") String name) {
+		/**
+		 *  만일 n이라는 이름의 URL 파라미터가 없는 경우
+		 *  400 Bad Request 에러가 발생한다.
+		 */
+		return "UserController.update(" + name + ")";
 	}
+	
+	@ResponseBody
+	@RequestMapping("/update2")
+	public String update2(@RequestParam(value="n", required=true, defaultValue="") String name) {
+		return "UserController.update(" + name + ")";
+	}
+	
+	@ResponseBody
+	@RequestMapping("/list")
+	public String list(@RequestParam(value="p", required=true, defaultValue="1") int pageNo) {
+		return "UserController.list(" + pageNo + ")";
+	}
+	
+	
 }
