58fed4386d5cb468bbcb62d0868145c4bfc4cc14
jun6988 <114986356+jun6988@users.noreply.github.com>
2022-11-24 09:43:30 +0900

    IoC Container #1

---
+++ container/src/main/java/com/bitacademy/container/user/User01.java
@@ -0,0 +1,11 @@
+package com.bitacademy.container.user;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class User01 {
+	public String getName() {
+		return "둘리";
+	}
+	
+}
